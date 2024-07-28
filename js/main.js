07b507da29b02b71a09fd587bc8dbf80b063cbf2
Jaydev1999 <jaydevkhatri7878@gmail.com>
2024-07-28 12:53:04 +0530

    1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 AOS.init();
 AOS.init({
-  
-  offset: 120, 
-  delay: 0, 
-  duration: 700, 
-  easing: 'ease', 
-  once: false, 
-  mirror: false, 
-  anchorPlacement: 'top-bottom', 
+  offset: 120,
+  delay: 0,
+  duration: 700,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   // Function to add and remove active class for touch events
+  const toggler = document.querySelector('.navbar-toggler');
+  toggler.addEventListener('click', function () {
+    this.classList.toggle('active');
+  });
   const addTouchEvents = (elements, activeClass) => {
-    elements.forEach(element => {
+    elements.forEach((element) => {
       element.addEventListener('touchstart', () => {
         element.classList.add(activeClass);
       });
